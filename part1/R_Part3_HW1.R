7402ee083eadb7a51697d3bfa82c3bd278066b6b
aza <azalea.m@pm.me>
2022-01-12 15:08:05 -0600

    scatterplot h v w by gender

--- part1/R_Part3_HW1.R
+++ part1/R_Part3_HW1.R
@@ -1,8 +1,15 @@
 View(InfantData1)
 library(tidyverse)
+library(scales)
 library(ggplot2)
 InfantData1 <- read.csv("~/InfantData1.csv")
 ## Updating theme, 
 theme_set(theme_light())
 theme_update(plot.title = element_text(hjust = 0.5))
 summary(InfantData1)
+#scatterplot
+ggplot(InfantData1, aes(HeightIn, WeightLbs, color = factor(Sex))) + 
+  geom_point() +
+  scale_x_continuous(expand = c(0, 0), limits = c(13, 25))  +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 12), labels = unit_format(unit = "lbs", scale = 1))
+
