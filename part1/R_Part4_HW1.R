8ef29fcdcc24b9efc20266cd3e1eb7b6a4aefefc
aza <azalea.m@pm.me>
2022-01-14 13:22:48 -0600

    Treemap was done in Tableau to compare what it looks like in R to make sure its right which just proves i shouldnt be doing this in R but im h

--- part1/R_Part4_HW1.R
+++ part1/R_Part4_HW1.R
@@ -1,8 +1,23 @@
 library(tidyverse)
 library(scales)
 library(ggplot2)
+library(treemapify)
+
 gmcar_price <- read.delim("~gmcar_price.txt")
 View(gmcar_price)
 ## Updating theme, 
 theme_set(theme_light())
 theme_update(plot.title = element_text(hjust = 0.5))
+## cleaning data
+gmcar_price<- gmcar_price[colSums(!is.na(gmcar_price)) > 0]
+## A. Treemap
+ # making new dataframe with relevant info
+df <- gmcar_price %>% 
+  group_by(Make, Model)%>%
+  summarise(Average_Price = mean(Price, na.rm=TRUE))
+
+ggplot(df, aes(area = Average_Price, fill = Average_Price, label = paste(Model, format(Average_Price), sep = "\n"), subgroup = Make, subgroup2 = Model)) + 
+  geom_treemap() +
+  labs(title = "Average Price of Recently Sold Car Make and Models")+
+  geom_treemap_text(colour = "white", grow = FALSE, size = 10) +
+  scale_fill_steps(low = "navy", high = "darkorange")
