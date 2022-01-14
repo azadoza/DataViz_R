2b0193767d080cd7a93f8ac3efe113a4573fbd6a
aza <azalea.m@pm.me>
2022-01-14 13:59:20 -0600

    treemap done... it's done. working on BI treemap next but ?

--- part1/R_Part4_HW1.R
+++ part1/R_Part4_HW1.R
@@ -2,22 +2,29 @@ library(tidyverse)
 library(scales)
 library(ggplot2)
 library(treemapify)
+library(viridis)
 
 gmcar_price <- read.delim("~gmcar_price.txt")
 View(gmcar_price)
 ## Updating theme, 
 theme_set(theme_light())
 theme_update(plot.title = element_text(hjust = 0.5))
+
 ## cleaning data
 gmcar_price<- gmcar_price[colSums(!is.na(gmcar_price)) > 0]
-## A. Treemap
+
+######## A. Treemap ##########
  # making new dataframe with relevant info
 df <- gmcar_price %>% 
   group_by(Make, Model)%>%
   summarise(Average_Price = mean(Price, na.rm=TRUE))
 
+ # Treemap plot by make and model
 ggplot(df, aes(area = Average_Price, fill = Average_Price, label = paste(Model, format(Average_Price), sep = "\n"), subgroup = Make, subgroup2 = Model)) + 
   geom_treemap() +
   labs(title = "Average Price of Recently Sold Car Make and Models")+
   geom_treemap_text(colour = "white", grow = FALSE, size = 10) +
+  geom_treemap_subgroup_border(colour = "white", size = 5) +
+  geom_treemap_subgroup_text(colour = "white", place = "bottom", grow = FALSE, alpha = 0.5) + 
   scale_fill_steps(low = "navy", high = "darkorange")
+  
