11c5125ea5a95eb19585098d35096015520642f1
aza <azalea.m@pm.me>
2022-01-31 11:41:33 -0600

    fixed the graph 3a

--- part2/R_Part2_3.R
+++ part2/R_Part2_3.R
@@ -3,13 +3,17 @@ library(lubridate)
 library(ggplot2)
 library(scales)
 theme_set(theme_bw())
-theme_update(plot.title = element_text(hjust = 0.5))
+theme_update(plot.title = element_text(hjust = 0.5, size = 13), 
+             panel.background = element_blank(),
+             plot.subtitle = element_text(size = 10),
+             panel.grid.minor = element_blank())
 
 View(MontanaPopulationData)
+
 ######### A ###########
 ggplot(MontanaPopulationData, aes(x=Year, y= Population))+
   geom_line(size=2)+
-  scale_y_continuous(trans = "log", labels = scales::label_number_si())+
+  scale_y_continuous(trans = "log", breaks = pretty_breaks(), labels = scales::label_number_si())+
   scale_x_continuous(n.breaks = 12)+
   theme(panel.grid.minor = element_blank())+
   labs(title = "Population of Montana Every Ten Years from 1890 to 2000", subtitle = "Plotted Logarithmically to Display Population Doubling")
