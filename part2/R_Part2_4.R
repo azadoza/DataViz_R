17b019f1241dc0557493b5fb3f78eb018f4948e0
aza <azalea.m@pm.me>
2022-02-03 11:20:04 -0600

    cleaned up graphs for pdf. cant get the EC with sphereplot

--- part2/R_Part2_4.R
+++ part2/R_Part2_4.R
@@ -12,9 +12,9 @@ theme_update(plot.title = element_text(hjust = 0.5, size = 13),
 ################################
 ggplot(MessierData[which(MessierData$Kind != ""),], aes(x= Distance..LY., y=Kind))+
   geom_violin(size=1, 
-              color="slateblue",
+              color="navyblue",
               fill = "grey90")+
-  geom_jitter(height = 0.1, width = 0, color = "grey40")+
+  geom_jitter(height = 0.1, width = 0, color = "slateblue4")+
   scale_x_log10(labels = scales::label_number_si())+
   labs(title = "Distribution of Distance in Light Years by Kind of Messier Object",
        x= "Log Base10_ Distance",
@@ -63,3 +63,11 @@ ggplot(MessierData,
     panel.grid.minor = element_line(colour = "#685F79")
   ) +
   scale_size_continuous(range = c(3, 7))
+
+MessierData$Apparent.Magnitude
+install.packages("sphereplot")
+install.packages("shiny")
+install.packages("manipulateWidget")
+library(sphereplot) 
+rgl.sphgrid(radius = 1, col.long='red', col.lat='blue', deggap = 15, longtype = "H",
+            add = FALSE, radaxis=TRUE, radlab='Radius')
