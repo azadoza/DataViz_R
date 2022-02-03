92ff119b023034d2ed8ffdb8f2d0b4c33f0ca566
aza <azalea.m@pm.me>
2022-02-03 09:52:31 -0600

    part d done but hiding size variation..

--- part2/R_Part2_4.R
+++ part2/R_Part2_4.R
@@ -37,3 +37,29 @@ ggplot(MessierData, aes(x=Apparent.Magnitude, y=Distance..LY., color = Apparent.
     panel.grid.major = element_line(colour = "#433858"), 
     panel.grid.minor = element_line(colour = "#685F79")
   )
+
+################################
+#### D. SCATTERPLOT part2 #####
+################################
+ggplot(MessierData, 
+       aes(
+         x=Apparent.Magnitude, 
+         y=Distance..LY., 
+         color = Apparent.Magnitude, 
+         size = Size..... )
+       )+
+  geom_point(shape = 18)+
+  scale_y_log10(labels = scales::label_number_si())+
+  labs(
+    title = "Apparent Magnitude vs Distance in Relation to Size of Messier Objects",
+       x="Apparent Magnitude",
+       y="Distance (LY)",
+       size = "Size"
+    )+
+  scale_color_gradient(low = "gold", high="#7c381f", guide=FALSE)+
+  theme(
+    panel.background = element_rect(fill = "black"),
+    panel.grid.major = element_line(colour = "#433858"), 
+    panel.grid.minor = element_line(colour = "#685F79")
+  ) +
+  scale_size_continuous(range = c(3, 7))
