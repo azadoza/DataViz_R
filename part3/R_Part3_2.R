66a17023707c217cd01f746264560f406567e701
aza <azalea.m@pm.me>
2022-02-16 18:37:57 -0600

    finished 2 with r, 2b has two diff graphs unsure of which to use in pdf

--- part3/R_Part3_2.R
+++ part3/R_Part3_2.R
@@ -1,15 +1,43 @@
+library(ggplot2)
 library(leaflet) 
 library(rgdal) 
-library(dplyr) 
+library(dplyr)
+
+#Shapefile
 policeBeatShape <- readOGR("~/Police_Beat_Boundary_View-shp")
 policeBeatShape@data$BEAT_OF_OCCURRENCE = as.integer(as.character(policeBeatShape@data$BEAT_NUMBE))
-chicago_crashes <- read.csv("~/chicago_crashes.csv")
 
+# CSV with beat info
+chicago_crashes <- read.csv("~/chicago_crashes.csv")
 View(chicago_crashes)
+
+## This shows us a bit about all accidents
+ # No NA or invalid data shows up in summary,
+ # no need for data cleanup
 summary(chicago_crashes$BEAT_OF_OCCURRENCE)
+
+## Aggregation 
+ # gives simple df that shows how many crashes occured per beat
 crashes.a <- chicago_crashes %>% 
   count(BEAT_OF_OCCURRENCE)
 
+## Adding the counts to the shapefile into new crash.map df
 crash.map <- merge(policeBeatShape, crashes.a, 
                    by.x  = "BEAT_OF_OCCURRENCE", 
                    by.y="BEAT_OF_OCCURRENCE")
+
+## Map Plot
+#### PLOT A. WITH MAP DETAILS ####
+leaflet(crash.map) %>%
+  addTiles() %>%  
+  addPolygons(weight = 1, 
+              smoothFactor = 0.5,
+              color = ~pal(n),    
+              opacity = 1.0, 
+              fillOpacity = .8,
+              )
+#### PLOT B WITHOUT MAP DETAILS ####
+leaflet(crash.map) %>%
+  addPolygons(weight = 1, smoothFactor = 0.5,
+              color = ~pal(n),    
+              opacity = 1.0, fillOpacity = 0.5)
